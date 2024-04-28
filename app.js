4916f643ab0e61deb6cd77e3fc7fd516d3408b5f
정영일 <bbangwon@outlook.com>
2024-04-28 17:32:05 +0900

    캠프그라운드 모델 기초

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 import express from 'express';
 import { fileURLToPath } from "url";
+import mongoose from 'mongoose';
+import Campground from './models/campground.js';
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("데이터베이스에 연결했습니다.");
+});
 
 const app = express();
 
 const viewsFolder = fileURLToPath(new URL("./views", import.meta.url));
 app.set('views', viewsFolder);
-
-
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title: '내 블랙야드', description: '저렴한 캠프장입니다.'});
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen(3000, () => {
