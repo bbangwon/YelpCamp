c882f8f8a7d682513a17986b1015f365b19785f4
youngil <youngil@placeb.com>
2024-05-08 13:37:03 +0900

    등록 경로

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import catchAsync from '../utils/catchAsync.js';
 import User from '../models/user.js';
 
 const router = express.Router();
@@ -7,15 +8,22 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
-    console.log(req.body);   
+router.post('/register', catchAsync(async (req, res) => {
+    try{
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
 
-    res.send(req.body);
-    
-    // res.send({
-    //     success: true,
-    //     msg: req.body
-    //   });
-});
+        req.flash('success', `Yelp Camp에 오신 것을 환영합니다! ${registeredUser.username}`);
+        res.send({
+            success: true,
+            msg: '사용자 등록에 성공했습니다.',
+            id: registeredUser._id
+        });
+    } catch(e){
+        req.flash('error', e.message);
+        throw new Error(e.message);
+    }    
+}));
 
 export default router;
