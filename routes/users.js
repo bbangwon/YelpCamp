7997c6e75444c56de2305b4be8a323c90a3dc6cb
정영일 <bbangwon@outlook.com>
2024-05-08 23:26:03 +0900

    회원가입, 로그인은 폼으로 직접 처리

--- routes/users.js
+++ routes/users.js
@@ -12,18 +12,15 @@ router.get('/register', (req, res) => {
 router.post('/register', catchAsync(async (req, res) => {
     try {
         const { email, username, password } = req.body;
+        console.log(email, username, password);
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
 
-        req.flash('success', `Yelp Camp에 오신 것을 환영합니다! ${registeredUser.username}`);
-        res.send({
-            success: true,
-            msg: '사용자 등록에 성공했습니다.',
-            id: registeredUser._id
-        });
+        req.flash('success', `Yelp Camp에 오신 것을 환영합니다! ${registeredUser.username}님!`);
+        res.redirect('/campgrounds');
     } catch (e) {
         req.flash('error', e.message);
-        throw new Error(e.message);
+        res.redirect('/register');
     }
 }));
 
@@ -33,12 +30,13 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', {
     failureFlash: '로그인에 실패했습니다. 다시 시도하세요.',
+    failureRedirect: '/login',
 }), (req, res) => {
-    req.flash('success', '환영합니다!');
-    res.send({
-        success: true,
-        msg: '로그인에 성공했습니다.'
-    });
+
+    console.log(req.user);
+
+    req.flash('success', `Yelp Camp에 오신 것을 환영합니다! ${req.user.username}님!`);
+    res.redirect('/campgrounds');
 });
 
 
