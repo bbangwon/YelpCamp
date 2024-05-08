7cce9335b5087e2cb0f01ff7a9dc56b0449f94d6
정영일 <bbangwon@outlook.com>
2024-05-08 22:59:44 +0900

    로그인 경로

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import catchAsync from '../utils/catchAsync.js';
 import User from '../models/user.js';
+import passport from 'passport';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', catchAsync(async (req, res) => {
-    try{
+    try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
@@ -20,10 +21,25 @@ router.post('/register', catchAsync(async (req, res) => {
             msg: '사용자 등록에 성공했습니다.',
             id: registeredUser._id
         });
-    } catch(e){
+    } catch (e) {
         req.flash('error', e.message);
         throw new Error(e.message);
-    }    
+    }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', {
+    failureFlash: '로그인에 실패했습니다. 다시 시도하세요.',
+}), (req, res) => {
+    req.flash('success', '환영합니다!');
+    res.send({
+        success: true,
+        msg: '로그인에 성공했습니다.'
+    });
+});
+
 
 export default router;
