2e713370a3909330b1cd12d31cd83395f8355152
youngil <youngil@placeb.com>
2024-05-09 17:30:46 +0900

    허가 리뷰

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,15 +2,15 @@ import express from 'express';
 import catchAsync from '../utils/catchAsync.js';
 import Campground from '../models/campground.js';
 import Review from '../models/review.js';
-import { validateReview } from '../middleware.js';
+import { validateReview, isLoggedIn } from '../middleware.js';
 
 const router = express.Router({ mergeParams: true});
 
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -20,7 +20,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
       id: id });
   }));
   
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
