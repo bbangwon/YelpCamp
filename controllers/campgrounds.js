6232e10a75d04189edfa5c073904f91bedcbaa62
youngil <youngil@placeb.com>
2024-05-09 19:17:50 +0900

    캠프그라운드 컨트롤러로 리펙토링

---
+++ controllers/campgrounds.js
@@ -0,0 +1,68 @@
+import Campground from '../models/campground.js';
+
+export default {
+    index: async (req, res) => {
+        const campgrounds = await Campground.find({});
+        res.render('campgrounds/index', { campgrounds });
+    },
+    renderNewForm: (req, res) => {
+        res.render('campgrounds/new');
+    },
+    createCampground: async (req, res, next) => {
+        const campground = new Campground(req.body);
+        campground.author = req.user._id;
+        await campground.save();
+        req.flash('success', '캠핑장을 추가했습니다.');
+        res.send({
+            success: true,
+            msg: '캠핑장을 추가했습니다.',
+            id: campground._id
+        });
+    },
+    showCampground: async (req, res) => {
+        const { id } = req.params;
+        const campground = await Campground.findById(id).populate({
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        }).populate('author');
+        if (!campground) {
+            req.flash('error', '캠핑장을 찾을 수 없습니다.');
+            return res.redirect('/campgrounds');
+        }
+        res.render('campgrounds/show', { campground });
+    },
+    renderEditForm: async (req, res) => {
+        const { id } = req.params;
+
+        const campground = await Campground.findById(id);
+        if (!campground) {
+            req.flash('error', '캠핑장을 찾을 수 없습니다.');
+            return res.redirect('/campgrounds');
+        }
+
+        res.render('campgrounds/edit', { campground });
+    },
+    updateCampground: async (req, res) => {
+        const { id } = req.params;
+        await Campground.findByIdAndUpdate(id, req.body);
+        req.flash('success', '캠핑장을 수정했습니다.');
+        res.send({
+            success: true,
+            msg: '캠핑장을 수정했습니다.',
+            id: id
+        });
+    },
+    deleteCampground: async (req, res) => {
+        const { id } = req.params;
+        await Campground.findByIdAndDelete(id);
+        req.flash('success', '캠핑장을 삭제했습니다.');
+        res.send({
+            success: true,
+            msg: '캠핑장을 삭제했습니다.',
+            id: id
+        });
+    }
+}
+
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,83 +1,25 @@
 import express from 'express';
+import campgrounds from '../controllers/campgrounds.js';
 import catchAsync from '../utils/catchAsync.js';
-import Campground from '../models/campground.js';
 import { isLoggedIn, validateCampground, isAuthor } from '../middleware.js';
 
 const router = express.Router();
 
 //목록
-router.get('/', async (req, res) => {
-  const campgrounds = await Campground.find({});
-  res.render('campgrounds/index', { campgrounds });
-});
+router.get('/', campgrounds.index);
 
 //추가
-router.get('/new', isLoggedIn, (req, res) => {
-  res.render('campgrounds/new');
-});
-
-router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-  const campground = new Campground(req.body);
-  campground.author = req.user._id;
-  await campground.save();
-  req.flash('success', '캠핑장을 추가했습니다.');
-  res.send({
-    success: true,
-    msg: '캠핑장을 추가했습니다.',
-    id: campground._id
-  });
-}));
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 //세부정보
-router.get('/:id', catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findById(id).populate({
-    path: 'reviews',
-    populate: { 
-      path: 'author'
-    }
-  }).populate('author');
-  if(!campground){
-    req.flash('error', '캠핑장을 찾을 수 없습니다.');
-    return res.redirect('/campgrounds');
-  }
-  res.render('campgrounds/show', { campground });
-}));
+router.get('/:id', catchAsync(campgrounds.showCampground));
 
 //수정
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  
-  const campground = await Campground.findById(id);  
-  if(!campground){
-    req.flash('error', '캠핑장을 찾을 수 없습니다.');
-    return res.redirect('/campgrounds');
-  }
-
-  res.render('campgrounds/edit', { campground });
-}));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndUpdate(id, req.body);
-  req.flash('success', '캠핑장을 수정했습니다.');
-  res.send({
-    success: true,
-    msg: '캠핑장을 수정했습니다.',
-    id: id
-  });
-}));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
 //삭제
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  req.flash('success', '캠핑장을 삭제했습니다.');
-  res.send({
-    success: true,
-    msg: '캠핑장을 삭제했습니다.',
-    id: id
-  });
-}));
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 export default router;
