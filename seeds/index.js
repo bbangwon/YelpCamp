53aa9dd5e60503979feccd43dbe18e398e2f4a14
정영일 <bbangwon@outlook.com>
2024-05-13 01:30:56 +0900

    seed 파일 수정 - 맵 위치 추가

--- seeds/index.js
+++ seeds/index.js
@@ -21,11 +21,18 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            author: '663c2bf922312bbb9e91f92b', //admin user id
+            author: '663f3a894f08039da80c08e0', //admin user id
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(seedHelper.descriptors)} ${sample(seedHelper.places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.',
             price,
+            geometry: {
+                type: 'Point',
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ]
+            },
             images:[
                 {
                   url: 'https://res.cloudinary.com/dibhzzbts/image/upload/v1715419071/YelpCamp/mnqb0doh0qeneoigkiey.jpg',
