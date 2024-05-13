2e41472c0660dbb784829897124191501bdec43c
정영일 <bbangwon@outlook.com>
2024-05-13 22:23:27 +0900

    사용자 지정 팝업 추가

--- models/campground.js
+++ models/campground.js
@@ -11,7 +11,9 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
-const campgroundSchema = new Schema({
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
     geometry: {
@@ -38,9 +40,15 @@ const campgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p>${this.description.substring(0, 20)}...</p>`;
 });
 
-campgroundSchema.post('findOneAndDelete', async function (doc) {
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
         await Review.deleteMany({
             _id: {
@@ -50,4 +58,4 @@ campgroundSchema.post('findOneAndDelete', async function (doc) {
     }
 })
 
-export default mongoose.model('Campground', campgroundSchema);
+export default mongoose.model('Campground', CampgroundSchema);
